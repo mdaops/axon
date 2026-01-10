420ae096c0247377276036354ebae4dc7665cb3b
michael <michael@mabbott.dev>
2026-01-10 12:27:32 +0000

    feat: initial axon monorepo setup with turborepo and bun

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Axon - Synapse Product Monorepo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+            turbo
+            nodejs_22
+
+            kubectl
+            kustomize
+
+            just
+            jq
+          ];
+
+          shellHook = ''
+            echo "Axon - Synapse Product Monorepo"
+          '';
+        };
+      }
+    );
+}
